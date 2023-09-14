48b56e52f812ea0a2c222ab7f99bc700fc4d7966
“lizilong” <2659248601@qq.com>
2023-09-14 19:10:44 +0800

    nihao

---
+++ src/main/java/com/leo/demo/TestController.java
@@ -0,0 +1,25 @@
+package com.leo.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Title: TestController
+ * @Author lzl
+ */
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+
+    @GetMapping("/get")
+    public String get() {
+
+        System.out.println("nihao LEO");
+
+        return "nihao LEO";
+    }
+
+}
