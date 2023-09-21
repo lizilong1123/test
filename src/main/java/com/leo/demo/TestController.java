e8b0b6149d9d8643dd70de5ad89e4e504781694c
“lizilong” <2659248601@qq.com>
2023-09-21 13:13:40 +0800

    更新

--- src/main/java/com/leo/demo/TestController.java
+++ src/main/java/com/leo/demo/TestController.java
@@ -42,7 +42,7 @@ public class TestController {
     public String run() {
 
         System.out.println("好赞啊 LEO");
-        System.out.println("好赞啊 LEO");
+        System.out.println("好niu啊 LEO");
 
         return "好赞啊 LEO";
 
