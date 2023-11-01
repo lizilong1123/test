baa42d654e89a4a0c4c57aaddea80c8567e26e7c
“lizilong” <2659248601@qq.com>
2023-11-01 09:51:46 +0800

    添加健康检查接口

---
+++ src/main/java/com/leo/demo/HomeController.java
@@ -0,0 +1,47 @@
+package com.leo.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author zhaoqt
+ */
+@RestController
+@RequestMapping
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "hello";
+    }
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "ping";
+    }
+
+    @RequestMapping("/open/questionnaire")
+    public String questionnaire() {
+        return "ping";
+    }
+
+
+    @GetMapping("/livenessProbe")
+    public String livenessProbe() {
+        return "ok";
+    }
+
+    @GetMapping("/readinessProbe")
+    public String readinessProbe() {
+        return "ok";
+    }
+
+    @GetMapping("/startupProbe")
+    public String startupProbe() {
+        return "ok";
+    }
+
+
+}
