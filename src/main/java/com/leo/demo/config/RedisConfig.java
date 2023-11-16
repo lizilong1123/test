abef11f90caec5fad46d1baa32f69a6d6b569ca8
“lizilong” <2659248601@qq.com>
2023-11-16 13:53:33 +0800

    update

--- src/main/java/com/leo/demo/config/RedisConfig.java
+++ src/main/java/com/leo/demo/config/RedisConfig.java
@@ -31,40 +31,40 @@ import java.util.TimeZone;
 @Configuration
 public class RedisConfig {
 
-//    public ObjectMapper objectMapper() {
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.setTimeZone(TimeZone.getTimeZone("GMT+8"));
-//        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-//        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-//        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
-//        objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
-//        return objectMapper;
-//    }
-//
-//    @Bean
-//    @ConditionalOnMissingBean
-//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-//        // 创建RedisTemplate<String, Object>对象
-//        RedisTemplate<String, Object> template = new RedisTemplate<>();
-//        template.setConnectionFactory(factory);
-//
-//        // 定义Jackson2JsonRedisSerializer序列化对象
-//        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-//        jackson2JsonRedisSerializer.setObjectMapper(objectMapper());
-//
-//        StringRedisSerializer stringSerial = new StringRedisSerializer();
-//        // redis key 序列化方式使用stringSerial
-//        template.setKeySerializer(stringSerial);
-//        // redis value 序列化方式使用jackson
-//        template.setValueSerializer(jackson2JsonRedisSerializer);
-//        // redis hash key 序列化方式使用stringSerial
-//        template.setHashKeySerializer(stringSerial);
-//        // redis hash value 序列化方式使用jackson
-//        template.setHashValueSerializer(jackson2JsonRedisSerializer);
-//
-//        template.afterPropertiesSet();
-//        return template;
-//    }
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setTimeZone(TimeZone.getTimeZone("GMT+8"));
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+        return objectMapper;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        // 创建RedisTemplate<String, Object>对象
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        // 定义Jackson2JsonRedisSerializer序列化对象
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper());
+
+        StringRedisSerializer stringSerial = new StringRedisSerializer();
+        // redis key 序列化方式使用stringSerial
+        template.setKeySerializer(stringSerial);
+        // redis value 序列化方式使用jackson
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        // redis hash key 序列化方式使用stringSerial
+        template.setHashKeySerializer(stringSerial);
+        // redis hash value 序列化方式使用jackson
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
 //
 //    @Bean
 //    public RedisConnectionFactory lettuceConnectionFactory(RedisProperties redisProperties) {
@@ -80,16 +80,7 @@ public class RedisConfig {
 //        return new LettuceConnectionFactory(redisSentinelConfiguration, lettuceClientConfiguration);
 //    }
 
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-        return template;
-    }
+
 
 }
 
