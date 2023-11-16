0155ac10308c9a17fc944db4fa5814d0f10c6894
“lizilong” <2659248601@qq.com>
2023-11-16 14:36:02 +0800

    update

--- src/main/java/com/leo/demo/config/RedisConfig.java
+++ src/main/java/com/leo/demo/config/RedisConfig.java
@@ -1,86 +1,23 @@
 package com.leo.demo.config;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import io.lettuce.core.ReadFrom;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisSentinelConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.text.SimpleDateFormat;
-import java.util.HashSet;
-import java.util.TimeZone;
-
-/**
- * @author ygr
- * @date 2022-02-15 16:30
- */
-@Slf4j
 @Configuration
 public class RedisConfig {
 
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setTimeZone(TimeZone.getTimeZone("GMT+8"));
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
-        objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
-        return objectMapper;
-    }
-
     @Bean
-    @ConditionalOnMissingBean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-        // 创建RedisTemplate<String, Object>对象
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(factory);
-
-        // 定义Jackson2JsonRedisSerializer序列化对象
-        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-        jackson2JsonRedisSerializer.setObjectMapper(objectMapper());
-
-        StringRedisSerializer stringSerial = new StringRedisSerializer();
-        // redis key 序列化方式使用stringSerial
-        template.setKeySerializer(stringSerial);
-        // redis value 序列化方式使用jackson
-        template.setValueSerializer(jackson2JsonRedisSerializer);
-        // redis hash key 序列化方式使用stringSerial
-        template.setHashKeySerializer(stringSerial);
-        // redis hash value 序列化方式使用jackson
-        template.setHashValueSerializer(jackson2JsonRedisSerializer);
-
-        template.afterPropertiesSet();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
     }
-//
-//    @Bean
-//    public RedisConnectionFactory lettuceConnectionFactory(RedisProperties redisProperties) {
-//        RedisSentinelConfiguration redisSentinelConfiguration = new RedisSentinelConfiguration(
-//                redisProperties.getSentinel().getMaster(), new HashSet<>(redisProperties.getSentinel().getNodes())
-//        );
-//
-//        LettucePoolingClientConfiguration lettuceClientConfiguration = LettucePoolingClientConfiguration.builder()
-//                // 读写分离，若主节点能抗住读写并发，则不需要设置，全都走主节点即可
-//                .readFrom(ReadFrom.ANY_REPLICA)
-//                .build();
-//
-//        return new LettuceConnectionFactory(redisSentinelConfiguration, lettuceClientConfiguration);
-//    }
-
-
-
 }
-
